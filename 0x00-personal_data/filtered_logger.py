6f9f57056a6f749855b0f69447c9809d513f35bf
Josephdada01 <ridwandada500@gmail.com>
2024-05-15 15:26:10 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,9 +14,31 @@ filter_datum should be less than 5 lines long and use re.sub to perform
 the substitution with a single regex.
 """
 import re
+import logging
+from typing import Dict, List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """function can be used to anonymize sensitive information in log files"""
     pattern = f"({'|'.join(re.escape(f) for f in fields)})=([^ {separator}]+)"
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+    message = re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """mtd to filter values in incoming log records using filter_datum"""
+        message = super(RedactingFormatter, self).format(record)
+        redacting = filter_datum(self.fields, self.REDACTION,
+                                 message, self.SEPARATOR)
+        return redacting
