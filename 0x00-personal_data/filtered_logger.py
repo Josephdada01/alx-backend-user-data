510931b8fd4b2c2d25c3b341b15598e94b5a80af
Josephdada01 <ridwandada500@gmail.com>
2024-05-15 15:54:24 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,15 +15,15 @@ the substitution with a single regex.
 """
 import re
 import logging
-from typing import Dict, List
+from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """function can be used to anonymize sensitive information in log files"""
-    pattern = f"({'|'.join(re.escape(f) for f in fields)})=([^ {separator}]+)"
-    message = re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
-    return message
+    """Function to anonymize sensitive information in log files."""
+    escaped_fields = '|'.join(re.escape(f) for f in fields)
+    pattern = f"({escaped_fields})=([^ {re.escape(separator)}]+)"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
 
 
 class RedactingFormatter(logging.Formatter):
@@ -37,8 +37,10 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """mtd to filter values in incoming log records using filter_datum"""
-        message = super(RedactingFormatter, self).format(record)
-        redacting = filter_datum(self.fields, self.REDACTION,
-                                 message, self.SEPARATOR)
-        return redacting
+        """Mtd to filter values in incoming log records using filter_datum."""
+        original_message = record.msg
+        redacted_message = filter_datum(self.fields,
+                                        self.REDACTION,
+                                        original_message, self.SEPARATOR)
+        record.msg = redacted_message
+        return super(RedactingFormatter, self).format(record)
