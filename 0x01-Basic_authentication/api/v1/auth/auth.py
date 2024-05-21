432923a2f2815bb03d9e587932fa602f8d53ae74
Josephdada01 <ridwandada500@gmail.com>
2024-05-21 14:15:02 +0100

    implemented the classes as required

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Implementing a custom authentication"""
+from flask import request
+from typing import List, TypeVar
+from models.user import User
+
+
+class Auth:
+    """class to manage basic api"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """a public method"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """a public method"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ a public method"""
+        return None
