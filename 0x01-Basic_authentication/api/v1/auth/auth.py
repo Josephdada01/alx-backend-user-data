94deb2e548c5e98abf4d09b575e0acc4d77303d8
Josephdada01 <ridwandada500@gmail.com>
2024-05-21 17:50:36 +0100

    implemented Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -46,7 +46,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """a public methods"""
-        return None
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ a public method"""
