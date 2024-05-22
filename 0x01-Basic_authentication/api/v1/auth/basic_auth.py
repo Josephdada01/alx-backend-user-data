fc4a6d02ac9b733eb221b2306d35eb143fdc9407
Josephdada01 <ridwandada500@gmail.com>
2024-05-22 15:53:54 +0100

    implemented a function that returns the User instance based on his email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Basic Auth modules"""
+from typing import TypeVar, Optional
 from api.v1.auth.auth import Auth
+from models.user import User
 import base64
 
 
@@ -54,3 +56,21 @@ class BasicAuth(Auth):
         # Split the decoded string into email and password
         u_email, u_passwd = decoded_base64_authorization_header.split(':', 1)
         return u_email, u_passwd
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on his email and password."""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        # Using the class method search of the User class
+        users = User.search({"email": user_email})
+        if not users:
+            return None
+        # Assuming search returns a list of users, take the first one
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
