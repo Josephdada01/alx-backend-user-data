8d54e2fb67f18aca5b788bb460a48e07d8777aea
Josephdada01 <ridwandada500@gmail.com>
2024-05-28 19:38:09 +0100

    implemnt a fuction that create a session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,3 +72,17 @@ class Auth:
                 return False
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """This method creates a session ID for the
+        user identified by the email."""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=session_id)
+                return session_id
+            else:
+                return None
+        except NoResultFound:
+            return None
