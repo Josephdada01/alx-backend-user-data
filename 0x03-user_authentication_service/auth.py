b9306c27b4dbed4f5eab011e325bf6c615ff5001
Josephdada01 <ridwandada500@gmail.com>
2024-05-28 13:31:14 +0100

    made adjustment

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,3 +17,42 @@ def _hash_password(password: str) -> bytes:
 
     hashed_password = bcrypt.hashpw(password_bytes, salt)
     return hashed_password
+
+def find_user_by(self, **kwargs) -> User:
+        """
+        This method takes in arbitrary keyword arguments and returns the first
+        row found in the users table as filtered by the method’s
+        input arguments
+        """
+        # Query the User table with the provided keyword arguments
+        all_users = self.__session.query(User)
+        for key, val in kwargs.items():
+            if key not in User.__dict__:
+                # if the query is bad raise Invlid request
+                raise InvalidRequestError
+            for user in all_users:
+                if getattr(user, key) == val:
+                    return user
+        # raise no result if no result is found
+        raise NoResultFound
+
+    def update_user(self, user_id: int, **kwargs):
+        """
+        method that takes as argument a required user_id integer and
+        arbitrary keyword arguments, and returns None.
+        The method will use find_user_by to locate the user to update,
+        then will update the user’s attributes as passed in the method’s
+        arguments then commit changes to the database.
+        If an argument that does not correspond to a user attribute is passed,
+        raise a ValueError
+        """
+        try:
+            usr = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError()
+        for k, v in kwargs.items():
+            if hasattr(usr, k):
+                setattr(usr, k, v)
+            else:
+                raise ValueError
+        self._session.commit()
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -43,42 +43,3 @@ class DB:
         self._session.commit()
         # Return the user instance
         return new_user
-
-    def find_user_by(self, **kwargs) -> User:
-        """
-        This method takes in arbitrary keyword arguments and returns the first
-        row found in the users table as filtered by the method’s
-        input arguments
-        """
-        # Query the User table with the provided keyword arguments
-        all_users = self.__session.query(User)
-        for key, val in kwargs.items():
-            if key not in User.__dict__:
-                # if the query is bad raise Invlid request
-                raise InvalidRequestError
-            for user in all_users:
-                if getattr(user, key) == val:
-                    return user
-        # raise no result if no result is found
-        raise NoResultFound
-
-    def update_user(self, user_id: int, **kwargs):
-        """
-        method that takes as argument a required user_id integer and
-        arbitrary keyword arguments, and returns None.
-        The method will use find_user_by to locate the user to update,
-        then will update the user’s attributes as passed in the method’s
-        arguments then commit changes to the database.
-        If an argument that does not correspond to a user attribute is passed,
-        raise a ValueError
-        """
-        try:
-            usr = self.find_user_by(id=user_id)
-        except NoResultFound:
-            raise ValueError()
-        for k, v in kwargs.items():
-            if hasattr(usr, k):
-                setattr(usr, k, v)
-            else:
-                raise ValueError
-        self._session.commit()
