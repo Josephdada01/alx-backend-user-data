7e31a50d0db46baa6e07901d1315c2df450cd18b
Josephdada01 <ridwandada500@gmail.com>
2024-05-28 16:32:05 +0100

    fixed the app.y to meet teh requirement

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-"""The app modules"""
+"""app.py modules"""
 from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'], strict_slashes=False)
-def app() -> str:
-    """app route that return"""
+@app.route("/", methods=["GET"])
+def home():
+    """defining the home route"""
     return jsonify({"message": "Bienvenue"})
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
