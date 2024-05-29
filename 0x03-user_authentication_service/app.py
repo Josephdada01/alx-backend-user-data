55023084ec60943cfc87370bbb38cfa9aa874c30
Josephdada01 <ridwandada500@gmail.com>
2024-05-29 11:04:07 +0100

    fixing bug

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,23 +29,5 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route("/sessions", methods=["POST"])
-def login() -> str:
-    """a login function to respond to the POST /sessions route"""
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    if not (Auth.valid_login(email, password)):
-        abort(401)
-    else:
-        # creating a new session for the user
-        session_id = Auth.create_session(email)
-        # storing the session ID as a cookie with key "session_id" on
-        # the response and returning JSON payload of the form
-        response = jsonify({"email": email, "message": "logged in"})
-        response.set_cookie("session_id", session_id)
-        return response
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
