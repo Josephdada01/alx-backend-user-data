477df48c7c9695454956ca0019017efda91a724c
Josephdada01 <ridwandada500@gmail.com>
2024-05-29 11:23:41 +0100

    fixed the bug

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
-Auth = Auth()
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -28,22 +28,24 @@ def users() -> str:
     except Exception:
         return jsonify({"message": "email already registered"}), 400
 
+
 @app.route('/sessions', methods=["POST"])
 def login() -> str:
     """a login function to respond to the POST /sessions route"""
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if not (Auth.valid_login(email, password)):
+    if not (AUTH.valid_login(email, password)):
         abort(401)
     else:
         # creating a new session for the user
-        session_id = Auth.create_session(email)
+        session_id = AUTH.create_session(email)
         # storing the session ID as a cookie with key "session_id" on
         # the response and returning JSON payload of the form
         response = jsonify({"email": email, "message": "logged in"})
         response.set_cookie("session_id", session_id)
         return response
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
